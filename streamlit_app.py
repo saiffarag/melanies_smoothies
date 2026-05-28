1ff7bf49412c4e962cbfec0a594f5856e4464b98
Saif Farag <125253070+saiffarag@users.noreply.github.com>
2026-05-28 12:11:45 -0600

    Fetch and display watermelon data from API
    Added a request to fetch watermelon data from Smoothiefroot API and display it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,9 @@ if ingredients_list:
 
 
 
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
 
         
